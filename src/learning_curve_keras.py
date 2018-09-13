6f92feb01776ec6a71935011ffc8b0400d811df6
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-09-13 13:52:41 +0200

    train Keras model on any corpus restricting to first x minutes of audio data

--- src/learning_curve_keras.py
+++ src/learning_curve_keras.py
@@ -108,16 +108,15 @@ def generate_subsets(corpus_id, language, batch_size, feature_type='mfcc', num_m
         table = [
             ['', 'Train-Set', 'Dev-Set', 'Test-Set'],
             ['# speech segments', train_it.n, dev_it.n, test_it.n],
-            ['# batches', len(train_it), len(dev_it), len(test_it)],
-            ['total audio length'] + [timedelta(seconds=subset.audio_total_length) for subset in
-                                      [train_it, dev_it, test_it]]
+            [f'# batches (batch_size={batch_size})', len(train_it), len(dev_it), len(test_it)],
+            ['total audio length'] + [timedelta(seconds=subset.audio_total_length) for subset in [train_it, dev_it, test_it]]
 
         ]
         print(tabulate(table, headers='firstrow'))
         print()
         return train_it, dev_it, test_it
 
-    raise ValueError('no precomputed HDF5-features found in {corpus_root}!')
+    raise ValueError(f'no precomputed HDF5-features found in {corpus_root}!')
 
 
 def find_precomputed_features(corpus_root, feature_type):

6185112c3514677cd0fcc5a4dc0291af26feb09f
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-09-14 16:25:38 +0200

    add GPU 3

--- src/learning_curve_keras.py
+++ src/learning_curve_keras.py
@@ -23,7 +23,7 @@ from util.train_util import get_target_dir
 # some Keras/TF setup
 # os.environ['CUDA_VISIBLE_DEVICES'] = "2"
 config = tf.ConfigProto()
-config.gpu_options.visible_device_list = "2"
+config.gpu_options.visible_device_list = "2,3"
 config.gpu_options.allow_growth = True
 session = tf.Session(config=config)
 K.set_session(session)

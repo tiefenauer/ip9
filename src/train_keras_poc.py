fix number of units in BRNN layer make SampleGenerator return associative arrays include ground truth in generated samples

src/train_keras_poc.py
@@ -6,6 +6,7 @@ import numpy as np
 import scipy
 import tensorflow as tf
 from keras import backend as K
+from keras.callbacks import TensorBoard
 from keras.optimizers import Adam
 from keras_preprocessing.image import Iterator
 from keras_preprocessing.sequence import pad_sequences
@@ -52,14 +53,20 @@ def main():
     model.summary()
 
     opt = Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-8, clipnorm=5)
-    model.compile(loss={'ctc': ctc_dummy_loss, 'decoder': decoder_dummy_loss},
-                  optimizer=opt,
-                  metrics={'decoder': ler},  #
-                  loss_weights=[1, 0]  # only optimize CTC cost
-                  )
+    model.compile(optimizer=opt, loss=ctc_dummy_loss)
 
     report_cb = ReportCallback(model, val_gen, target_dir)
-    model.fit_generator(train_gen, validation_data=val_gen, epochs=10000, callbacks=[report_cb])
+    tb_cb = TensorBoard(log_dir=target_dir, write_graph=False, write_images=True)
+    callbacks = [report_cb, tb_cb]
+    model.fit_generator(generator=train_gen,
+                        validation_data=val_gen,
+                        epochs=10000,
+                        callbacks=callbacks,
+                        initial_epoch=0,
+                        verbose=1,
+                        class_weight=None,
+                        max_queue_size=10,
+                        workers=1)
 
 
 class SampleGenerator(Iterator):
@@ -74,6 +81,8 @@ class SampleGenerator(Iterator):
 
         self.Y_lengths = np.array([len(sample.text)])
 
+        self.source_str = np.array([sample.text])
+
         labels_encoded = [encode(sample.text)]
         rows, cols, data = [], [], []
         for row, label in enumerate(labels_encoded):
@@ -84,7 +93,18 @@ class SampleGenerator(Iterator):
         self.Y = scipy.sparse.coo_matrix((data, (rows, cols)), dtype=np.int16)
 
     def _get_batches_of_transformed_samples(self, index_array):
-        return [self.X, self.Y, self.X_lengths, self.Y_lengths], [np.zeros((self.X.shape[0],)), self.Y]
+        inputs = {
+            'the_input': self.X,
+            'the_labels': self.Y,
+            'input_length': self.X_lengths,
+            'label_length': self.Y_lengths,
+            'source_str': self.source_str
+        }
+        outputs = {
+            'ctc': np.zeros([self.X.shape[0]])
+        }
+        return inputs, outputs
+        # return [self.X, self.Y, self.X_lengths, self.Y_lengths], [np.zeros((self.X.shape[0],)), self.Y]
 
     def next(self):
         with self.lock:

src/util/brnn_util.py
@@ -5,11 +5,12 @@ Contains various helper functions to create/train a BRNN
 import tensorflow as tf
 from keras import Input, Model, backend as K
 from keras.activations import relu
+from keras.initializers import random_normal
 from keras.layers import TimeDistributed, Dense, Activation, Dropout, Bidirectional, SimpleRNN, Lambda
 from keras.utils import get_custom_objects
 
 
-def deep_speech_model(num_features, num_hidden=2048, dropout=0.1, num_classes=28):
+def deep_speech_model(num_features, num_hidden=2048, rnn_size=512, dropout=0.1, num_classes=28):
     """
     Deep Speech model with architecture as described in the paper:
         5 Layers (3xFC + 1xBRNN + 1xFC) with Dropout applied to FC layer
@@ -23,63 +24,53 @@ def deep_speech_model(num_features, num_hidden=2048, dropout=0.1, num_classes=28
     Reference: [1] https://arxiv.org/abs/1412.5567
     """
     get_custom_objects().update({"clipped_relu": clipped_relu})
-    x = Input(name='inputs', shape=(None, num_features))
-    o = x
+    K.set_learning_phase(1)
 
-    # First layer
-    o = TimeDistributed(Dense(num_hidden))(o)
-    o = TimeDistributed(Activation(clipped_relu))(o)
-    o = TimeDistributed(Dropout(dropout))(o)
+    input_data = Input(name='the_input', shape=(None, num_features))
 
-    # Second layer
-    o = TimeDistributed(Dense(num_hidden))(o)
-    o = TimeDistributed(Activation(clipped_relu))(o)
-    o = TimeDistributed(Dropout(dropout))(o)
+    # 3 FC layers with clipped ReLU followed by a Dropout
+    init = random_normal(stddev=0.046875)
+    x = TimeDistributed(
+        Dense(num_hidden, name='fc1', kernel_initializer=init, bias_initializer=init, activation=clipped_relu))(
+        input_data)
+    # o = TimeDistributed(Activation(clipped_relu))(o)
+    x = TimeDistributed(Dropout(dropout))(x)
+    x = TimeDistributed(
+        Dense(num_hidden, name='fc2', kernel_initializer=init, bias_initializer=init, activation=clipped_relu))(x)
+    # o = TimeDistributed(Activation(clipped_relu))(o)
+    x = TimeDistributed(Dropout(dropout))(x)
+    x = TimeDistributed(
+        Dense(num_hidden, name='fc3', kernel_initializer=init, bias_initializer=init, activation=clipped_relu))(x)
+    # o = TimeDistributed(Activation(clipped_relu))(o)
+    x = TimeDistributed(Dropout(dropout))(x)
 
-    # Third layer
-    o = TimeDistributed(Dense(num_hidden))(o)
-    o = TimeDistributed(Activation(clipped_relu))(o)
-    o = TimeDistributed(Dropout(dropout))(o)
+    # BRNN layer
+    x = Bidirectional(SimpleRNN(rnn_size, return_sequences=True, activation=clipped_relu, dropout=dropout,
+                                kernel_initializer='he_normal'), merge_mode='sum')(x)
+    # o = TimeDistributed(Dropout(dropout))(o)
 
-    # Fourth layer
-    o = Bidirectional(SimpleRNN(num_hidden, return_sequences=True,
-                                dropout=dropout,
-                                activation=clipped_relu,
-                                kernel_initializer='he_normal'), merge_mode='sum')(o)
-    o = TimeDistributed(Dropout(dropout))(o)
-
-    # Fifth layer
-    o = TimeDistributed(Dense(num_hidden))(o)
-    o = TimeDistributed(Activation(clipped_relu))(o)
-    o = TimeDistributed(Dropout(dropout))(o)
+    # another FC layer with dropout
+    x = TimeDistributed(Dense(num_hidden))(x)
+    x = TimeDistributed(Activation(clipped_relu))(x)
+    x = TimeDistributed(Dropout(dropout))(x)
 
     # Output layer
-    o = TimeDistributed(Dense(num_classes, name='y_pred', activation='softmax'), name='outputs')(o)
+    y_pred = TimeDistributed(
+        Dense(num_classes, name='y_pred', kernel_initializer=init, bias_initializer=init, activation='softmax'))(x)
 
-    return ctc_model(x, o)
-
-
-def ctc_model(inputs, output):
-    """
-    Create compilable model for input and output tensors
-    :param inputs: input tensor of the model
-    :param output: output tensor of the model
-    :return: instance of keras.engine.training.Model to compile with optimizer and loss
-    """
     # Input placeholders for true labels and input sequence lengths (need to be fed during training/validation)
-    labels = Input(name='labels', shape=(None,), dtype='int32', sparse=True)
-    inputs_length = Input(name='inputs_length', shape=[1], dtype='int32')
-    labels_length = Input(name='labels_length', shape=[1], dtype='int32')
+    labels = Input(name='the_labels', shape=(None,), dtype='int32', sparse=True)
+    input_length = Input(name='input_length', shape=[1], dtype='int32')
+    label_length = Input(name='label_length', shape=[1], dtype='int32')
 
     # Output layer for decoded label (values are integers)
-    dec = Lambda(decoder_lambda_func, arguments={'is_greedy': False}, name='decoder')
-    y_pred = dec([output, inputs_length])
+    # dec = Lambda(decoder_lambda_func, arguments={'is_greedy': False}, name='decoder')
+    # y_pred = dec([output, inputs_length])
 
-    # Output layer for CTC-loss
-    ctc = Lambda(ctc_lambda_func, output_shape=(1,), name="ctc")
-    loss = ctc([labels, output, inputs_length, labels_length])
+    # CTC-loss
+    loss_out = Lambda(ctc_lambda_func, output_shape=(1,), name="ctc")([y_pred, labels, input_length, label_length])
 
-    return Model(inputs=[inputs, labels, inputs_length, labels_length], outputs=[loss, y_pred])
+    return Model(inputs=[input_data, labels, input_length, label_length], outputs=loss_out)
 
 
 def decoder_lambda_func(args, is_greedy=False, beam_width=100, top_paths=1, merge_repeated=True):
@@ -162,9 +153,9 @@ def ctc_lambda_func(args):
 
     # hack: we need to import tensorflow here again to make keras.engine.saving.load_model work...
     import tensorflow as tf
-    y_true, y_pred, input_length, label_length = args
-    y_true = tf.sparse_tensor_to_dense(y_true)
-    return K.ctc_batch_cost(y_true, y_pred, input_length, label_length)
+    y_pred, labels, input_length, label_length = args
+    labels = tf.sparse_tensor_to_dense(labels)
+    return K.ctc_batch_cost(labels, y_pred, input_length, label_length)
 
 
 def clipped_relu(x):
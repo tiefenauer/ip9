d2f4fec75631d980c3080dc4aee8fa31c3182d75
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-09-19 09:11:16 +0200

    create HDF5 features when creating CSV-corpus

--- src/corpus2csv.py
+++ src/corpus2csv.py
@@ -1,10 +1,14 @@
 import argparse
-from os import listdir, makedirs
+from os import listdir, makedirs, remove
 from os.path import join, exists, getsize
 from shutil import rmtree
 
+import h5py
+import numpy as np
 import pandas
 import soundfile as sf
+from python_speech_features import mfcc
+from scipy.io import wavfile
 from tqdm import tqdm
 
 from util.corpus_util import get_corpus
@@ -15,8 +19,12 @@ parser.add_argument('-id', type=str, help='target-ID for processed files')
 parser.add_argument('-c', '--corpus', type=str, choices=['rl', 'ls'], help='corpus to export')
 parser.add_argument('-l', '--language', type=str, help='language to use')
 parser.add_argument('-t', '--target_dir', type=str, help='target directory to save results')
+parser.add_argument('-num', '--include_numeric', action='store_true', default=False,
+                    help='(optional) whether to include transcripts with numeric chars (default: False)')
 parser.add_argument('-m', '--max', nargs='?', type=int, default=None,
                     help='(optional) maximum number of speech segments to process')
+parser.add_argument('-f', '--force', action='store_true',
+                    help='(optional) force override existing files. Default: False')
 args = parser.parse_args()
 
 
@@ -29,7 +37,7 @@ def main():
         print(f'target directory {target_dir} does not exist. Creating...')
         makedirs(target_dir)
 
-    if listdir(target_dir):
+    if args.force and listdir(target_dir):
         override = input(f"""WARNING: target directory {target_dir} already exists. Override? 
         (this will remove all files in {target_dir}!!!) (Y/n)        
         """)
@@ -46,16 +54,19 @@ def main():
 
 
 def extract_speech_segments(corpus_id, corpus, target_dir):
-    train_set, dev_set, test_set = corpus.train_dev_test_split(include_numeric=True)
+    train_set, dev_set, test_set = corpus.train_dev_test_split(include_numeric=args.include_numeric)
 
     print(f'processing training segments')
-    process_subset('train', train_set, corpus_id, target_dir)
+    df_train = process_subset('train', train_set, corpus_id, target_dir)
 
     print(f'processing validation segments')
-    process_subset('dev', dev_set, corpus_id, target_dir)
+    df_valid = process_subset('dev', dev_set, corpus_id, target_dir)
 
     print(f'processing validation segments')
-    process_subset('test', test_set, corpus_id, target_dir)
+    df_test = process_subset('test', test_set, corpus_id, target_dir)
+
+    print(f'pre-computing features')
+    compute_features(df_train, df_valid, df_test, target_dir)
 
 
 def process_subset(subset_id, subset, corpus_id, target_dir):
@@ -64,6 +75,7 @@ def process_subset(subset_id, subset, corpus_id, target_dir):
     csv_path = join(target_dir, f'{corpus_id}-{subset_id}.csv')
     print(f'saving metadata in {csv_path}')
     df.to_csv(csv_path, index=False)
+    return df
 
 
 def split_speech_segments(subset, corpus_id, subset_id, target_dir):
@@ -87,6 +99,39 @@ def split_speech_segments(subset, corpus_id, subset_id, target_dir):
         files.append((wav_path, getsize(wav_path), segment.text))
 
     return pandas.DataFrame(data=files, columns=['wav_filename', 'wav_filesize', 'transcript'])
+
+
+def compute_features(df_train, df_valid, df_test, target_dir):
+    h5_file_path = join(target_dir, 'features_mfcc.h5')
+    if exists(h5_file_path):
+        remove(h5_file_path)
+    with h5py.File(h5_file_path) as h5_file:
+        create_subset(h5_file, 'train', df_train)
+        create_subset(h5_file, 'test', df_valid)
+        create_subset(h5_file, 'valid', df_test)
+
+
+def create_subset(h5_file, name, df):
+    h5_file.create_dataset(f'{name}/features', shape=(0,), maxshape=(None,), dtype=h5py.special_dtype(vlen=np.float32))
+    h5_file.create_dataset(f'{name}/labels', shape=(0,), maxshape=(None,), dtype=h5py.special_dtype(vlen=str))
+    h5_file.create_dataset(f'{name}/durations', shape=(0,), maxshape=(None,))
+    progress = tqdm(zip(df['wav_filename'], df['wav_filesize'], df['transcript']), total=len(df.index))
+    for wav_file_path, wav_file_size, transcript in progress:
+        progress.set_description(f'{name}: {wav_file_path}')
+        inputs = h5_file[name]['features']
+        labels = h5_file[name]['labels']
+        durations = h5_file[name]['durations']
+
+        rate, audio = wavfile.read(wav_file_path)
+        inp = mfcc(audio, samplerate=rate, numcep=26)  # (num_timesteps x num_features)
+        inputs.resize(inputs.shape[0] + 1, axis=0)
+        inputs[inputs.shape[0] - 1] = inp.flatten().astype(np.float32)
+
+        labels.resize(labels.shape[0] + 1, axis=0)
+        labels[labels.shape[0] - 1] = transcript
+
+        durations.resize(durations.shape[0] + 1, axis=0)
+        durations[durations.shape[0] - 1] = wav_file_size
 
 
 def get_segment_id(segment):

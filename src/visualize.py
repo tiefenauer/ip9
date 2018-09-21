6aafcc99d56ce66edd289c6d0ad858428b8f10ff
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-09-21 16:34:13 +0200

    visualize multiple CSV files with metrics for various amounts of training data (learning curve)

--- src/visualize.py
+++ src/visualize.py
@@ -1,4 +1,6 @@
 import argparse
+import re
+from glob import glob
 from os import listdir
 from os.path import join, abspath
 
@@ -8,10 +10,36 @@ import seaborn as sns
 sns.set()
 import matplotlib.pyplot as plt
 
+csv_pattern = re.compile('model_(\d*)_min\.csv')
+
+
+def visualize_learning_curve(root_dir, metric='WER'):
+    print(f'visualizing learning curve from {root_dir}')
+    csv_files = dict()
+    for csv_path in glob(join(root_dir, '*.csv')):
+        matches = re.findall(csv_pattern, csv_path)
+        if matches:
+            minutes = int(matches[0])
+            csv_files[f'{minutes} minute' + ('s' if minutes > 1 else '')] = csv_path
+
+    fig, ax = plt.subplots(1, 1, figsize=(16, 9))
+    for label, csv_path in csv_files.items():
+        df = pd.read_csv(csv_path).reset_index()
+        df['index'] += 1
+        df.plot(x='index', y=metric, ax=ax, label=label)
+
+    ax.set_xticks(range(1, len(df.index) + 1))
+    ax.set_title(f'{metric} for {root_dir}')
+    ax.set_xlabel(f'Epoch')
+    ax.set_ylabel(f'{metric}')
+
+    plt.show()
+
 
 def visualize(root_dir):
     print(f'visualizing results from {root_dir}')
-    csv_path = join(root_dir, 'wer_ler.csv')
+
+    csv_path = join(root_dir, 'model.csv')
     df = pd.read_csv(csv_path).reset_index()
     df['index'] += 1
 
@@ -44,4 +72,5 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     root_dir = args.dir if args.dir else get_root_dir()
-    visualize(root_dir)
+    # visualize(root_dir)
+    visualize_learning_curve(root_dir, metric='LER')

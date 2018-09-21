0a5b1d80b5bb164fdd7a19021c021ca463d89972
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-09-21 17:19:08 +0200

    refactor code to visualize both single run or learning curve without code duplication

--- src/visualize.py
+++ src/visualize.py
@@ -1,6 +1,6 @@
 import argparse
 import re
-from glob import glob, iglob
+from glob import iglob
 from os import listdir
 from os.path import join, abspath
 
@@ -11,21 +11,29 @@ sns.set()
 import matplotlib.pyplot as plt
 
 
-def visualize_learning_curve(root_dir, metric='WER'):
+def visualize_single_run(csv_path):
+    print(f'visualizing results from {csv_path}')
+
+    df = pd.read_csv(csv_path).reset_index()
+    fig, ax = plt.subplots(1, 1, figsize=(16, 9))
+
+    plot_dataframe(df, 'WER', csv_path, ax=ax)
+    plot_dataframe(df, 'LER', csv_path, ax=ax)
+
+    plt.show()
+
+
+def visualize_learning_curves(root_dir):
     print(f'visualizing learning curve from {root_dir}')
     csv_files = map_files_to_minutes(root_dir)
 
-    fig, ax = plt.subplots(1, 1, figsize=(16, 9))
+    fig, (ax_wer, ax_ler) = plt.subplots(2, 1, figsize=(16, 16))
+
     for minutes, csv_path in csv_files.items():
         df = pd.read_csv(csv_path).reset_index()
-        df['index'] += 1
         label = f'{minutes} minute' + ('s' if minutes > 1 else '')
-        df.plot(x='index', y=metric, ax=ax, label=label)
-
-    ax.set_xticks(range(1, len(df.index) + 1))
-    ax.set_title(f'{metric} for {root_dir}')
-    ax.set_xlabel(f'Epoch')
-    ax.set_ylabel(f'{metric}')
+        ax_wer = plot_dataframe(df, 'WER', root_dir, ax=ax_wer, label=label)
+        ax_ler = plot_dataframe(df, 'LER', root_dir, ax=ax_ler, label=label)
 
     plt.show()
 
@@ -39,24 +47,20 @@ def map_files_to_minutes(root_dir):
     return dict((int(matches[0]), csv_path) for matches, csv_path in matched_files if matches)
 
 
-def visualize(root_dir):
-    print(f'visualizing results from {root_dir}')
+def plot_dataframe(df, metric, data_source, ax=None, x='index', label=None):
+    if not ax:
+        fig, ax = plt.subplots(1, 1, figsize=(16, 9))
+    if not label:
+        label = metric
 
-    csv_path = join(root_dir, 'model.csv')
-    df = pd.read_csv(csv_path).reset_index()
     df['index'] += 1
-
-    fig, ax = plt.subplots(1, 1, figsize=(16, 9))
-    df.plot(x='index', y='WER', ax=ax)
-    df.plot(x='index', y='LER', ax=ax)
+    df.plot(x=x, y=metric, ax=ax, label=label)
 
     ax.set_xticks(range(1, len(df.index) + 1))
-    ax.set_title(f'WER/LER for {csv_path}')
+    ax.set_title(f'{metric} for {data_source}')
     ax.set_xlabel(f'Epoch')
-    ax.set_ylabel(f'WER/LER')
-
-    plt.show()
-    print('done!')
+    ax.set_ylabel(f'{metric}')
+    return ax
 
 
 def get_latest_dir(runs_dir):
@@ -65,7 +69,7 @@ def get_latest_dir(runs_dir):
 
 
 def get_root_dir(runs_dir=join('..', 'runs')):
-    root_dir = input('no root directory specified. Enter root directory now (or leave empty for newest)')
+    root_dir = input('no root directory specified. Enter root directory now (or leave empty for newest): ')
     return root_dir.strip() if root_dir else get_latest_dir(runs_dir)
 
 
@@ -75,5 +79,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     root_dir = args.dir if args.dir else get_root_dir()
-    # visualize(root_dir)
-    visualize_learning_curve(root_dir, metric='LER')
+
+    # model.csv contains the results of a single training run
+    if 'model.csv' in listdir(root_dir):
+        visualize_single_run(join(root_dir, 'model.csv'))
+
+    # model_[0-9]*_min.csv files contain the results of several training runs to plot a learning curve
+    else:
+        visualize_learning_curves(root_dir)

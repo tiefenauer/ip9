98fc8b4def33c0187cc74a54bd88cc305335a3e2
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-10-02 15:16:16 +0200

    add some fixes to LM jupyter notebook

--- src/lm/lm_util.py
+++ src/lm/lm_util.py
@@ -5,19 +5,19 @@ import nltk
 from util.string_util import replace_numeric, remove_punctuation, unidecode_keep_umlauts
 
 
-def process_line(line, min_words=4, language='german'):
+def process_line(line, language='german', min_words=4, ):
     sentences = []
     sents = nltk.sent_tokenize(line.strip(), language=language)
     for sentence in sents:
-        sentence_processed = process_sentence(sentence, min_words)
+        sentence_processed = process_sentence(sentence, language, min_words)
         if sentence_processed:
             sentences.append(sentence_processed)
 
     return sentences
 
 
-def process_sentence(sent, min_words=4):
-    words = [normalize_word(word) for word in nltk.word_tokenize(sent, language='german')]
+def process_sentence(sent, language, min_words=0):
+    words = [normalize_word(word) for word in nltk.word_tokenize(sent, language=language)]
     if len(words) >= min_words:
         return ' '.join(w for w in words if w).strip()  # prevent multiple spaces
     return ''

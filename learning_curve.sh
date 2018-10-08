c15cef532980c51462f26dc4793afd83c013a7e1
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-10-08 14:59:56 +0200

    print validation results again after every epoch to create WER/LER history over epochs

--- learning_curve.sh
+++ learning_curve.sh
@@ -10,9 +10,6 @@ where:
                                              The file must contain the words used for training delimited by space (no newlines)
     -t|--train_files <path>                  one or more comma-separated paths to CSV files containing the corpus files to use for training
     -v|--valid_files <path>                  one or more comma-separated paths to CSV files containing the corpus files to use for validation
-    -p|--val_printouts                       whether to print out samples from the validation set after each epoch.
-                                             Samples with a WER/LER < 0.5 will be printed. While this is good for illustrative purposes, it
-                                             will also significantly slow down training. (default: False)
     -g|--gpu <int>                           GPU to use
     -b|--batch_size <int>                    batch size (default: 16)
     -e|--epochs <int>                        number of epochs to train (default: 30)
@@ -33,7 +30,6 @@ gpu=''
 batch_size='16'
 epochs='30'
 valid_batches='0'
-val_printouts=false
 
 POSITIONAL=()
 while [[ $# -gt 0 ]]
@@ -100,10 +96,6 @@ case $key in
     shift
     shift
     ;;
-    -p|--val_printouts)
-    val_printouts=true
-    shift
-    ;;
     *)    # unknown option
     POSITIONAL+=("$1") # save it in an array for later
     shift
@@ -130,7 +122,6 @@ gpu             = ${gpu}
 batch_size      = ${batch_size}
 epochs          = ${epochs}
 valid_batches   = ${valid_batches}
-val_printouts   = ${val_printouts}
 -----------------------------------------------------
 " | tee ${lc_result_dir%/}/${lc_run_id}.log
 
@@ -167,7 +158,6 @@ do
         --valid_batches ${valid_batches} \
         --train_files ${train_files} \
         --valid_files ${valid_files} \
-        --val_printouts ${val_printouts} \
         2>&1 | tee ${lc_result_dir}/${run_id}.log # write to stdout and log file
 
     echo "

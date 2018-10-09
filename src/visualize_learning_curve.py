77a0770a072e31b1b9fb3180ef3efc9d4cd47b1a
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-10-09 11:08:23 +0200

    reduce code even more

--- src/visualize_learning_curve.py
+++ src/visualize_learning_curve.py
@@ -27,7 +27,6 @@ def main(args):
 def plot_losses(dfs_loss):
     styles = [[color + style for style in ['--', '-']] for color in ['r', 'g', 'b', 'c']]
     legends = ['training', 'validation']
-
     return plot_to_subplots(dfs_loss, styles, legends, 'CTC loss')
 
 
@@ -37,8 +36,10 @@ def plot_ler_wer(dfs_ler_wer):
     return plot_to_subplots(dfs_ler_wer, styles, legends, 'WER/LER')
 
 
-def plot_to_subplots(dfs, styles, legends, ylabel, xlabel='epochs',
-                     titles=['1 minute', '10 minutes', '100 minutes', '1,000 minutes']):
+def plot_to_subplots(dfs, styles, legends, ylabel, xlabel='epochs', titles=None):
+    if titles is None:
+        titles = ['1 minute', '10 minutes', '100 minutes', '1,000 minutes']
+
     fig, axes = plt.subplots(ncols=2, nrows=2, sharex=True, sharey=True, figsize=(14, 9))
 
     for df, ax, title, style in zip(dfs, axes.flatten(), titles, styles):
@@ -54,13 +55,6 @@ def plot_to_subplots(dfs, styles, legends, ylabel, xlabel='epochs',
     return fig, axes
 
 
-def create_title(key):
-    pattern = re.compile('(\d*)_min')
-    minutes = int(re.findall(pattern, key)[0])
-    unit = 'minutes' if minutes > 1 else 'minute'
-    return f'{minutes:,} {unit}'
-
-
 def setup(args):
     source_dir = abspath(args.source_dir)
     if not exists(source_dir):
@@ -72,40 +66,34 @@ def setup(args):
     return source_dir, target_dir
 
 
-def collect_data(source_dir):
-    df_loss, df_val_loss = collect_tensorboard_losses(source_dir)
-    df_ler, df_wer = collect_ler_wer(source_dir)
+def collect_data(source_dir, columns=None):
+    if columns is None:
+        columns = ['1_min', '10_min', '100_min', '1000_min']
+    df_loss, df_val_loss = collect_tensorboard_losses(source_dir, columns)
+    df_ler, df_wer = collect_ler_wer(source_dir, columns)
 
-    dfs_loss, dfs_ler_wer = [], []
-    for key in ['1_min', '10_min', '100_min', '1000_min']:
-        loss = df_loss[key]
-        val_loss = df_val_loss[key]
-        dfs_loss.append(pd.concat([loss, val_loss], axis=1))
-
-        wer_values = df_wer[key]
-        ler_values = df_ler[key]
-        dfs_ler_wer.append(pd.concat([ler_values, wer_values], axis=1))
+    dfs_loss = [pd.concat([df_loss[key], df_val_loss[key]], axis=1) for key in columns]
+    dfs_ler_wer = [pd.concat([df_ler[key], df_wer[key]], axis=1) for key in columns]
     return dfs_loss, dfs_ler_wer
 
 
-def collect_tensorboard_losses(source_dir):
-    df_loss = pd.DataFrame(columns=['1_min', '10_min', '100_min', '1000_min'])
-    df_val_loss = pd.DataFrame(columns=['1_min', '10_min', '100_min', '1000_min'])
-
-    for minutes, loss_csv in map_files_to_minutes(source_dir, 'run_', '-loss.csv').items():
-        df_loss[f'{minutes}_min'] = pd.read_csv(loss_csv)['Value'].values
-
-    for minutes, loss_csv in map_files_to_minutes(source_dir, 'run_', '-val_loss.csv').items():
-        df_val_loss[f'{minutes}_min'] = pd.read_csv(loss_csv)['Value'].values
-
-    df_loss.index += 1
-    df_val_loss.index += 1
+def collect_tensorboard_losses(source_dir, columns):
+    df_loss = read_tensorflow_loss(source_dir, '-loss.csv', columns)
+    df_val_loss = read_tensorflow_loss(source_dir, '-val_loss.csv', columns)
     return df_loss, df_val_loss
 
 
-def collect_ler_wer(source_dir):
-    df_ler = pd.DataFrame(columns=['1_min', '10_min', '100_min', '1000_min'])
-    df_wer = pd.DataFrame(columns=['1_min', '10_min', '100_min', '1000_min'])
+def read_tensorflow_loss(source_dir, postfix, columns):
+    df = pd.DataFrame(columns=columns)
+    for minutes, loss_csv in map_files_to_minutes(source_dir, 'run_', postfix).items():
+        df[f'{minutes}_min'] = pd.read_csv(loss_csv)['Value'].values
+    df.index += 1
+    return df
+
+
+def collect_ler_wer(source_dir, columns):
+    df_ler = pd.DataFrame(columns=columns)
+    df_wer = pd.DataFrame(columns=columns)
     for subdir_name in get_immediate_subdirectories(source_dir):
         for minutes, ler_wer_csv in map_files_to_minutes(join(source_dir, subdir_name), 'model_', '.csv').items():
             df_ler_wer = pd.read_csv(ler_wer_csv)

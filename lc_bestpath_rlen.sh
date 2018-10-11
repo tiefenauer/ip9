ef85d482efe576c669a05516a1d4e9490820ff22
Daniel Tiefenauer <git@tiefenauer.info>
2018-10-11 16:03:06 +0200

    add shell scripts from FHNW server

---
+++ lc_bestpath_rlen.sh
@@ -0,0 +1,2 @@
+cd ./src/
+./learning_curve.sh -r lc_bestpath_rlen -d /media/D1/daniel.tiefenauer/_runs/ -t /media/D1/daniel.tiefenauer/corpora/readylingua-en/readylingua-en-train.csv -v /media/D1/daniel.tiefenauer/corpora/readylingua-en/readylingua-en-dev.csv -x bestpath -l /media/D1/daniel.tiefenauer/lm/timit_en/libri-timit-lm.klm -a /media/D1/daniel.tiefenauer/lm/timit_en/libri-timit-lm.vocab -e 30

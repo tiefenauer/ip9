13a8add5038e714106bcadd431a6824fdc459ac5
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-10-17 17:27:17 +0200

    use new corpus files with relative paths

--- lc_rl_de_wiki.sh
+++ lc_rl_de_wiki.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-./learning_curve.sh -r lc_rl_de_wiki -d /media/D1/daniel.tiefenauer/_runs/ -t /media/D1/daniel.tiefenauer/corpora/readylingua-de/readylingua-de-train.csv -v /media/D1/daniel.tiefenauer/corpora/readylingua-de/readylingua-de-dev.csv -l /media/D1/daniel.tiefenauer/lm/wiki_de/wiki_de_5_gram_pruned.klm -a /media/D1/daniel.tiefenauer/lm/wiki_de/wiki_de_5_gram_pruned.vocab -e 30
+./learning_curve.sh -r lc_rl_de_wiki -d /media/D1/daniel.tiefenauer/_runs/ -t /media/D1/daniel.tiefenauer/corpora/readylingua-de/readylingua-de-train-rel.csv -v /media/D1/daniel.tiefenauer/corpora/readylingua-de/readylingua-de-dev-rel.csv -l /media/D1/daniel.tiefenauer/lm/wiki_de/wiki_de_5_gram_pruned.klm -a /media/D1/daniel.tiefenauer/lm/wiki_de/wiki_de_5_gram_pruned.vocab -e 30

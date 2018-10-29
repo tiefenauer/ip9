784503112b4f566b10fa0aadd7177ff5f55217e2
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-10-29 11:33:36 +0100

    save LibriSpeech corpus as CSV

--- src/create_ls_corpus.py
+++ src/create_ls_corpus.py
@@ -2,23 +2,22 @@
 import argparse
 import logging
 import math
-import os
 import re
 import sys
 from os import makedirs, walk
-from os.path import exists, splitext, join
-from pathlib import Path
+from os.path import exists, splitext, join, basename, pardir, abspath
 
-from pydub.utils import mediainfo
+import pandas as pd
+import soundfile as sf
+from tabulate import tabulate
 from tqdm import tqdm
+from unidecode import unidecode
 
 from constants import LS_SOURCE, LS_TARGET
-from corpus.corpus import LibriSpeechCorpus
-from corpus.corpus_entry import CorpusEntry
-from corpus.corpus_segment import SpeechSegment
 from util.audio_util import seconds_to_frame, to_wav
-from util.corpus_util import save_corpus, find_file_by_suffix
+from util.corpus_util import find_file_by_suffix
 from util.log_util import log_setup, create_args_str
+from util.string_util import normalize
 
 logfile = 'create_ls_corpus.log'
 log_setup(filename=logfile)
@@ -43,43 +42,6 @@ parser.add_argument('-o', '--overwrite', default=False, action='store_true',
                          'updated with the most current corpus entries. Default=False)')
 args = parser.parse_args()
 
-# -------------------------------------------------------------
-# Other values
-# -------------------------------------------------------------
-books_pattern = re.compile('(?P<book_id>\d+)'
-                           '\s*\|\s*'
-                           '(?P<book_title>.*?)'
-                           '\s*(\|\s*|\n)')
-speakers_pattern = re.compile('(?P<speaker_id>\d+)'
-                              '\s*\|\s*'
-                              '(?P<sex>[MF])'
-                              '\s*\|\s*'
-                              '(?P<subset>.*?)'
-                              '\s*\|\s*'
-                              '(?P<minutes>\d[\d.]*)'
-                              '\s*\|\s*'
-                              '(?P<speaker_name>.*)')
-chapters_pattern = re.compile("(?P<chapter_id>\d+)"
-                              "\s*\|\s*"
-                              "(?P<reader_id>\d+)"
-                              "\s*\|\s*"
-                              "(?P<minutes>\d[\d.]*)"
-                              "\s*\|\s*"
-                              "(?P<subset>.*?)"
-                              "\s*\|\s*"
-                              "(?P<project_id>\d+)"
-                              "\s*\|\s*"
-                              "(?P<book_id>\d+)"
-                              "\s*\|\s*"
-                              "(?P<chapter_title>.*)"
-                              "\s*\|\s*"
-                              "(?P<project_title>.*)")
-segment_pattern = re.compile('(?P<segment_id>.*)\s(?P<segment_start>.*)\s(?P<segment_end>.*)\n')
-
-non_ascii_pattern = re.compile(r'[^\x00-\x7F]+')
-punctuation_pattern = re.compile(r'[^\w\s]')
-whitespace_pattern = re.compile(r'\s+')
-
 
 def main():
     print(create_args_str(args))
@@ -96,240 +58,179 @@ def create_corpus(source_path, target_path, max_entries=None):
         print(f'creating target directory {target_path} as it does not exist yet')
         makedirs(target_path)
 
-    return create_librispeech_corpus(source_path=source_path, target_path=target_path, max_entries=max_entries)
+    return create_librispeech_corpus(source_dir=source_path, target_dir=target_path, max_entries=max_entries)
 
 
-def create_librispeech_corpus(source_path, target_path, max_entries):
-    audio_root = join(source_path, 'audio')
-    book_meta, chapter_meta, speaker_meta = collect_corpus_meta(audio_root)
+def create_librispeech_corpus(source_dir, target_dir, max_entries):
+    audio_root = join(source_dir, 'audio')
+    books_root = join(source_dir, 'books')
 
-    directories = [root for root, subdirs, files in walk(audio_root) if not subdirs]
+    chapters_file = find_file_by_suffix(audio_root, 'CHAPTERS.TXT')
+    chapter_meta = collect_chapter_meta(chapters_file)
+
+    book_texts = collect_book_texts(books_root)
+
+    directories = [root for root, subdirs, files in walk(audio_root) if not subdirs][:max_entries]
     progress = tqdm(directories, total=min(len(directories), max_entries or math.inf), file=sys.stderr, unit='entries')
 
     corpus_entries = []
-    for raw_path in progress:
-        if max_entries and len(corpus_entries) >= max_entries:
+    for source_dir in progress:
+        progress.set_description(f'{source_dir:{100}}')
+
+        chapter_id = basename(source_dir)
+        speaker_id = basename(abspath(join(source_dir, pardir)))
+
+        if chapter_id not in chapter_meta:
+            print(f'WARNING: no chapter information for chapter {chapter_id}. Skipping corpus entry...')
             break
 
-        progress.set_description(f'{raw_path:{100}}')
+        book_id = chapter_meta[chapter_id]['book_id']
+        if not book_id:
+            print(f'WARNING: no book text available for chapter {chapter_id}. Skipping corpus entry...')
+            break
 
-        parms = collect_corpus_entry_parms(raw_path, book_meta, chapter_meta, speaker_meta)
+        segments_file = find_file_by_suffix(source_dir, f'{speaker_id}-{chapter_id}.seg.txt')
+        if not segments_file:
+            log.warning(f'no segmentation found at {segments_file}. Skipping corpus entry...')
+            break
 
-        segments_file, transcript_file, mp3_file = collect_corpus_entry_files(raw_path, parms)
-        segments_file = join(raw_path, segments_file)
-        transcript_file = join(raw_path, transcript_file)
+        transcript_file = find_file_by_suffix(source_dir, f'{speaker_id}-{chapter_id}.trans.txt')
+        if not transcript_file:
+            log.warn(f'no transcript found at {transcript_file}. Skipping corpus entry...')
+            break
 
-        if not segments_file or not transcript_file or not mp3_file:
-            log.warning(f'Skipping directory (not all files found): {raw_path}')
+        mp3_file = find_file_by_suffix(source_dir, f'{chapter_id}.mp3')
+        if not mp3_file:
+            log.warn(f'no MP3 file found at {mp3_file}. Skipping corpus entry...')
             break
 
         segments = create_segments(segments_file, transcript_file)
 
-        # Convert and resample audio
-        wav_name = splitext(mp3_file)[0] + ".wav"
-        audio_dst = join(target_path, wav_name)
-        if not exists(audio_dst) or args.overwrite:
-            audio_src = join(raw_path, mp3_file)
-            to_wav(audio_src, audio_dst)
-            # audio, rate = librosa.load(audio_src, sr=16000, mono=True)
-            # audio, rate, segments = crop_to_segments(audio, rate, segments)  # uncomment to crop audio
-            # sf.write(audio_dst, audio, rate, 'PCM_16')
-        parms['media_info'] = mediainfo(audio_dst)
+        # crop/resample audio
+        wav_file = join(target_dir, basename(splitext(mp3_file)[0] + ".wav"))
+        if not exists(wav_file) or args.overwrite:
+            to_wav(mp3_file, wav_file)
+            crop_start = min(segment['start_frame'] for segment in segments)
+            crop_end = max(segment['end_frame'] for segment in segments)
+            audio, rate = sf.read(wav_file, start=crop_start, stop=crop_end)
+            sf.write(wav_file, audio, rate, 'PCM_16')
 
-        # Create corpus entry
-        corpus_entry = CorpusEntry(wav_name, segments, raw_path=raw_path, parms=parms)
-        corpus_entries.append(corpus_entry)
+            for segment in segments:
+                segment['start_frame'] -= crop_start
+                segment['end_frame'] -= crop_start
 
-    corpus = LibriSpeechCorpus(corpus_entries)
-    corpus_file = save_corpus(corpus, target_path)
-    return corpus, corpus_file
+        # write full transcript
+        with open(join(target_dir, f'{chapter_id}.txt'), 'w') as f:
+            book_text = normalize(book_texts[book_id], 'en')
+            first_transcript = segments[0]['transcript']
+            if first_transcript in book_text:
+                text_start = book_text.index(first_transcript)
+            else:
+                text_start = 0
+                # try to find the first transcript by searching the maximum substring from the left
+                for i in range(1, len(first_transcript) - 1):
+                    if first_transcript[:i] not in book_text:
+                        text_start = book_text.index(first_transcript[:i - 1])
+                        break
 
+            last_transcript = segments[-1]['transcript']
+            if last_transcript in book_text:
+                text_end = book_text.index(last_transcript) + len(last_transcript)
+            else:
+                # try to find last transcript by searching maximum substring from the right
+                text_end = len(book_text) - 1
+                for i in range(1, len(last_transcript) - 1):
+                    if last_transcript[-i:] not in book_text:
+                        text_end = book_text.index(last_transcript[-i + 1:]) + i - 1
+                        break
 
-def collect_corpus_meta(directory):
-    # books
-    books_file = find_file_by_suffix(directory, 'BOOKS.TXT')
-    books_file = join(directory, books_file)
-    book_meta = collect_book_meta(books_file)
+            f.write(book_text[text_start:text_end])
 
-    # chapters
-    chapters_file = find_file_by_suffix(directory, 'CHAPTERS.TXT')
-    chapters_file = join(directory, chapters_file)
-    chapter_meta = collect_chapter_meta(chapters_file)
+        for segment in segments:
+            subset = chapter_meta[chapter_id]['subset']
+            audio_file = basename(wav_file)
+            start_frame = segment['start_frame']
+            end_frame = segment['end_frame']
+            transcript = segment['transcript']
+            corpus_entries.append([subset, audio_file, start_frame, end_frame, transcript])
 
-    # speakers
-    speakers_file = find_file_by_suffix(directory, 'SPEAKERS.TXT')
-    speakers_file = join(directory, speakers_file)
-    speaker_meta = collect_speaker_meta(speakers_file)
-
-    return book_meta, chapter_meta, speaker_meta
-
-
-def collect_meta(file_path, pattern):
-    with open(file_path) as f:
-        for line in (line for line in f.readlines() if not line.startswith(';')):
-            results = re.search(pattern, line)
-            if results:
-                yield results
-
-
-def collect_book_meta(books_file):
-    book_meta = {}
-    for result in collect_meta(books_file, books_pattern):
-        book_id = result.group('book_id') if result.group('book_id') else 'unknown'
-        book_title = result.group('book_title') if result.group('book_title') else 'unknown'
-        book_meta[book_id] = book_title
-    book_meta['unknown'] = 'unknown book title'
-    return book_meta
+    corpus = pd.DataFrame(corpus_entries, columns=['subset', 'audio_file', 'start_frame', 'end_frame', 'transcript'])
+    corpus_index = join(target_dir, 'index.csv')
+    corpus.to_csv(corpus_index)
+    return corpus, corpus_index
 
 
 def collect_chapter_meta(chapters_file):
-    chapter_meta = {}
-    for result in collect_meta(chapters_file, chapters_pattern):
-        chapter_id = result.group('chapter_id')
-        chapter = {
-            'reader_id': result.group('reader_id'),
-            'length': float(result.group('minutes')),
-            'subset': result.group('subset'),
-            'project_id': result.group('project_id'),
-            'book_id': result.group('book_id'),
-            'chapter_title': result.group('chapter_title'),
-            'project_title': result.group('project_title')
-        }
-        chapter_meta[chapter_id] = chapter
-    chapter_meta['unknown'] = 'unknown chapter'
+    chapter_meta = {'unknown': 'unknown chapter'}
+
+    line_pattern = re.compile("(?P<chapter>\d+)\s*\|.*\|.*\|\s*(?P<subset>.*?)\s*\|.*\|\s*(?P<book>\d+)\s*\|.*\|.*")
+
+    with open(chapters_file) as f:
+        for line in (line for line in f.readlines() if not line.startswith(';')):
+            result = re.search(line_pattern, line)
+            subset = result.group('subset')
+            if '-clean-' in subset:
+                chapter_id = result.group('chapter')
+                book_id = result.group('book')
+                chapter_meta[chapter_id] = {'subset': subset, 'book_id': book_id}
+
     return chapter_meta
-
-
-def collect_speaker_meta(speakers_file):
-    speaker_meta = {}
-    for result in collect_meta(speakers_file, speakers_pattern):
-        speaker_id = result.group('speaker_id')
-        speaker = {
-            'sex': result.group('sex'),
-            'subset': result.group('subset'),
-            'length': float(result.group('minutes')),
-            'name': result.group('speaker_name'),
-        }
-        speaker_meta[speaker_id] = speaker
-    speaker_meta['unknown'] = 'unknown speaker'
-    return speaker_meta
 
 
 def collect_book_texts(books_root):
     book_texts = {}
+    invalid_encodings = []
     for root, files in tqdm([(root, files) for root, subdirs, files in walk(books_root)
                              if not subdirs and len(files) == 1], unit='books'):
-        book_path = join(root, files[0])
-        encoding = 'latin-1' if 'ascii' in book_path else 'utf-8'  # use latin-1 for ascii files because of encoding problems
+        book_file = join(root, files[0])
+        book_id = basename(root)
 
-        book_id = root.split(os.sep)[-1]
-        book_text = Path(book_path).read_text(encoding=encoding)
-        book_texts[book_id] = book_text
+        encoding = 'ascii' if 'ascii' in book_file else 'utf-8'
+        with open(book_file, 'r', encoding=encoding) as f:
+            try:
+                book_texts[book_id] = f.read().strip()
+            except UnicodeDecodeError as e:
+                invalid_encodings.append((book_id, book_file, encoding, e.start, e.end))
+
+    if invalid_encodings:
+        print(f'could not decode the following {len(invalid_encodings)} books because of decoding errors:')
+        print(tabulate(invalid_encodings, headers=['id', 'file', 'encoding', 'start', 'end']))
+        print('trying to fix those files by using Latin-1 encoding and removing invalid HTML markup')
+
+        for book_id, book_file, encoding, start, end in invalid_encodings:
+            with open(book_file, 'r', encoding='latin-1') as f:
+                book_text = f.read()
+                if '<pre>' in book_text:
+                    book_text = book_text[:book_text.index('<pre>') + 5]
+                if '</pre>' in book_text:
+                    book_text = book_text[:book_text.index('</pre>')]
+            with open(book_file, 'w', encoding='ascii') as f:
+                f.write(unidecode(book_text))
+
     return book_texts
-
-
-def collect_corpus_entry_parms(directory, book_meta, chapter_meta, speaker_meta):
-    files_pattern = re.compile("[\\\/]mp3[\\\/](?P<speaker_id>\d*)[\\\/](?P<chapter_id>\d*)")
-    result = re.search(files_pattern, directory)
-    if result:
-        speaker_id = result.group('speaker_id')
-        chapter_id = result.group('chapter_id')
-
-        chapter = chapter_meta[chapter_id] if chapter_id in chapter_meta else {'chapter_title': 'unknown',
-                                                                               'book_id': 'unknown',
-                                                                               'subset': 'unknown'}
-        speaker = speaker_meta[speaker_id] if speaker_id in speaker_meta else 'unknown'
-
-        book_id = chapter['book_id']
-
-        book_title = book_meta[book_id] if book_id in book_meta else chapter['project_title']
-        chapter_title = chapter['chapter_title']
-        subset = chapter['subset']
-        speaker_name = speaker['name']
-
-        return {'name': book_title,
-                'id': chapter_id,
-                'chapter_title': chapter_title,
-                'language': 'en',
-                'book_id': book_id,
-                'speaker_id': speaker_id,
-                'chapter_id': chapter_id,
-                'speaker_name': speaker_name,
-                'subset': subset}
-
-
-def collect_corpus_entry_files(directory, parms):
-    speaker_id = parms['speaker_id']
-    chapter_id = parms['chapter_id']
-    segments_file = find_file_by_suffix(directory, f'{speaker_id}-{chapter_id}.seg.txt')
-    transcript_file = find_file_by_suffix(directory, f'{speaker_id}-{chapter_id}.trans.txt')
-    mp3_file = find_file_by_suffix(directory, f'{chapter_id}.mp3')
-    return segments_file, transcript_file, mp3_file
-
-
-def normalize_text(text):
-    text = text.upper()
-    text = text.replace('-', ' ')
-    text = re.sub(non_ascii_pattern, '', text)
-    text = re.sub(punctuation_pattern, '', text)
-    text = re.sub(whitespace_pattern, ' ', text)
-    return text
-
-
-def find_text_between(prev_text, next_text, book_text):
-    prev_text = normalize_text(prev_text)
-    next_text = normalize_text(next_text)
-
-    if prev_text in book_text and next_text in book_text:
-        # find occurrences of prev_text and nex_text which are closes to each other
-        prev_indices = [(m.start(), m.end()) for m in re.finditer(prev_text, book_text)]
-        min_distance = math.inf
-        start = end = 0
-        for prev_start, prev_end in prev_indices:
-            next_indices = [(m.start(), m.end()) for m in re.finditer(next_text, book_text) if m.start() > prev_end]
-            for next_start, next_end in next_indices:
-                distance = next_start - prev_end
-                if distance < min_distance:
-                    min_distance = distance
-                    start = prev_end + 1
-                    end = next_start - 1
-
-        between_text = book_text[start:end]
-        return between_text
-    return None
 
 
 def create_segments(segments_file, transcript_file):
     segment_texts = {}
     with open(transcript_file, 'r') as f_transcript:
         for line in f_transcript.readlines():
-            id, segment_text = line.split(' ', 1)
-            segment_texts[id] = segment_text.replace('\n', '')
+            id, transcript = line.split(' ', 1)
+            segment_texts[id] = transcript.replace('\n', '')
+
+    line_pattern = re.compile('(?P<segment_id>.*)\s(?P<segment_start>.*)\s(?P<segment_end>.*)\n')
 
     segments = []
     with open(segments_file, 'r') as f_segments:
         lines = f_segments.readlines()
         for i, line in enumerate(lines):
-            id, start_frame, end_frame = parse_segment_line(line)
-            segment_text = segment_texts[id] if id in segment_texts else ''
-
-            speech = SpeechSegment(start_frame=start_frame,
-                                   end_frame=end_frame,
-                                   transcript=segment_text,
-                                   language='en')
-            segments.append(speech)
-
+            result = re.search(line_pattern, line)
+            if result:
+                id = result.group('segment_id')
+                start_frame = seconds_to_frame(result.group('segment_start'))
+                end_frame = seconds_to_frame(result.group('segment_end'))
+                transcript = normalize(segment_texts[id], 'en') if id in segment_texts else ''
+                segments.append({'start_frame': start_frame, 'end_frame': end_frame, 'transcript': transcript})
     return segments
-
-
-def parse_segment_line(line):
-    result = re.search(segment_pattern, line)
-    if result:
-        segment_id = result.group('segment_id')
-        segment_start = seconds_to_frame(result.group('segment_start'))
-        segment_end = seconds_to_frame(result.group('segment_end'))
-        return segment_id, segment_start, segment_end
-    return None, None, None
 
 
 if __name__ == '__main__':

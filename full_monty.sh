7a1d3bde562fb74cb8ffe4e3f0f9171b898e224d
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-10-30 11:15:06 +0100

    fix the full monty

--- full_monty.sh
+++ full_monty.sh
@@ -10,22 +10,22 @@ read gpu
 echo "training on ${corpus} corpus using GPU ${gpu} for all training runs!"
 
 if [[ ${gpu} = '' ]]; then
-    ./lc_cv_en_ds.sh --gpu ${gpu}
-    ./lc_cv_en_ds_adam.sh --gpu ${gpu}
-    ./lc_cv_en_ds_dropouts.sh --gpu ${gpu}
-    ./lc_cv_en_ds_dropouts_adam.sh --gpu ${gpu}
+    ./lc_cv_en_ds.sh ${gpu}
+    ./lc_cv_en_ds_adam.sh ${gpu}
+    ./lc_cv_en_ds_dropouts.sh ${gpu}
+    ./lc_cv_en_ds_dropouts_adam.sh ${gpu}
 
-    ./lc_cv_en_timit.sh --gpu ${gpu}
-    ./lc_cv_en_timit_adam.sh --gpu ${gpu}
-    ./lc_cv_en_timit_dropouts.sh --gpu ${gpu}
-    ./lc_cv_en_timit_dropouts_adam.sh --gpu ${gpu}
+    ./lc_cv_en_timit.sh ${gpu}
+    ./lc_cv_en_timit_adam.sh ${gpu}
+    ./lc_cv_en_timit_dropouts.sh ${gpu}
+    ./lc_cv_en_timit_dropouts_adam.sh ${gpu}
 else
-    ./lc_rl_de_wiki.sh --gpu ${gpu}
-    ./lc_rl_de_wiki_adam.sh --gpu ${gpu}
-    ./lc_rl_de_wiki_dropouts.sh --gpu ${gpu}
-    ./lc_rl_de_wiki_dropouts_adam.sh --gpu ${gpu}
-    ./lc_rl_de_wiki_synth.sh --gpu ${gpu}
-    ./lc_rl_de_wiki_synth_adam.sh --gpu ${gpu}
-    ./lc_rl_de_wiki_synth_dropouts.sh --gpu ${gpu}
-    ./lc_rl_de_wiki_synth_dropouts_adam.sh --gpu ${gpu}
+    ./lc_rl_de_wiki.sh ${gpu}
+    ./lc_rl_de_wiki_adam.sh ${gpu}
+    ./lc_rl_de_wiki_dropouts.sh ${gpu}
+    ./lc_rl_de_wiki_dropouts_adam.sh ${gpu}
+    ./lc_rl_de_wiki_synth.sh ${gpu}
+    ./lc_rl_de_wiki_synth_adam.sh ${gpu}
+    ./lc_rl_de_wiki_synth_dropouts.sh ${gpu}
+    ./lc_rl_de_wiki_synth_dropouts_adam.sh ${gpu}
 fi

cdb41686f7f2166c9907fbbc84b14c31b62e0766
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-11-19 17:13:50 +0100

    don't train using ADAM optimizer and don't train using timit LM

--- full_monty.sh
+++ full_monty.sh
@@ -11,23 +11,23 @@ echo "training on ${corpus} corpus using GPU ${gpu} for all training runs!"
 
 if [[ ${corpus} = 'cv' ]]; then
     ./lc_cv_en_ds.sh ${gpu}
-    ./lc_cv_en_ds_adam.sh ${gpu}
+#    ./lc_cv_en_ds_adam.sh ${gpu}
     ./lc_cv_en_ds_dropouts.sh ${gpu}
-    ./lc_cv_en_ds_dropouts_adam.sh ${gpu}
+#    ./lc_cv_en_ds_dropouts_adam.sh ${gpu}
 
-    ./lc_cv_en_timit.sh ${gpu}
-    ./lc_cv_en_timit_adam.sh ${gpu}
-    ./lc_cv_en_timit_dropouts.sh ${gpu}
-    ./lc_cv_en_timit_dropouts_adam.sh ${gpu}
+#    ./lc_cv_en_timit.sh ${gpu}
+#    ./lc_cv_en_timit_adam.sh ${gpu}
+#    ./lc_cv_en_timit_dropouts.sh ${gpu}
+#    ./lc_cv_en_timit_dropouts_adam.sh ${gpu}
 elif [[ ${corpus} = 'rl' ]]; then
     ./lc_rl_de_wiki.sh ${gpu}
-    ./lc_rl_de_wiki_adam.sh ${gpu}
+#    ./lc_rl_de_wiki_adam.sh ${gpu}
     ./lc_rl_de_wiki_dropouts.sh ${gpu}
-    ./lc_rl_de_wiki_dropouts_adam.sh ${gpu}
+#    ./lc_rl_de_wiki_dropouts_adam.sh ${gpu}
     ./lc_rl_de_wiki_synth.sh ${gpu}
-    ./lc_rl_de_wiki_synth_adam.sh ${gpu}
+#    ./lc_rl_de_wiki_synth_adam.sh ${gpu}
     ./lc_rl_de_wiki_synth_dropouts.sh ${gpu}
-    ./lc_rl_de_wiki_synth_dropouts_adam.sh ${gpu}
+#    ./lc_rl_de_wiki_synth_dropouts_adam.sh ${gpu}
 else
     echo "invalid value $corpus for corpus"
 fi

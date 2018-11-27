2e127a35ffc4f29db7c183697425ddb3ace4fc9a
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-11-27 08:25:50 +0100

    fix arguments in bash scripts

--- evaluate_pipeline_de.sh
+++ evaluate_pipeline_de.sh
@@ -15,8 +15,6 @@ echo ${me} > ${target_dir}/${me}.log
 
 cd ./src/
 python3 evaluate_pipeline_de.py \
-    --corpus rl \
-    --language de \
     --keras_path /home/daniel/Documents/_runs/lc_rl_de_wiki_synth/1000_min \
     --gpu ${gpu} \
     --target_dir /media/daniel/IP9/demos/performance_rl_de | tee ${target_dir}/${me}.log
--- evaluate_pipeline_en.sh
+++ evaluate_pipeline_en.sh
@@ -17,7 +17,7 @@ python3 evaluate_pipeline_en.py \
     --corpus /media/D1/daniel.tiefenauer/corpora/librispeech \
     --language en \
     --keras_path /media/D1/daniel.tiefenauer/_runs/lc_cv_en_ds_dropouts/1000_min \
-    --ds_path /media/D1/daniel.tiefenauer/asr/model/output_graph.pbmm \
+    --ds_path /media/D1/daniel.tiefenauer/asr/output_graph.pbmm \
     --ds_alpha_path /media/D1/daniel.tiefenauer/asr/alphabet.txt \
     --ds_trie_path /media/D1/daniel.tiefenauer/asr/model/trie \
     --lm_path /media/D1/daniel.tiefenauer/asr/lm.binary \

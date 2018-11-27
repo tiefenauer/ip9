f465e9c90c5ccf69aa1b17df25a2096250f80d4a
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-11-27 08:34:59 +0100

    fix trie path

--- evaluate_pipeline_en.sh
+++ evaluate_pipeline_en.sh
@@ -19,7 +19,7 @@ python3 evaluate_pipeline_en.py \
     --keras_path /media/D1/daniel.tiefenauer/_runs/lc_cv_en_ds_dropouts/1000_min \
     --ds_path /media/D1/daniel.tiefenauer/asr/output_graph.pbmm \
     --ds_alpha_path /media/D1/daniel.tiefenauer/asr/alphabet.txt \
-    --ds_trie_path /media/D1/daniel.tiefenauer/asr/model/trie \
+    --ds_trie_path /media/D1/daniel.tiefenauer/asr/trie \
     --lm_path /media/D1/daniel.tiefenauer/asr/lm.binary \
     --gpu ${gpu} \
     --target_dir ${target_dir} | tee ${target_dir}/${me}.log

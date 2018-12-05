abe8c943de67bcef9fcdb93763bd57bd0389813f
Daniel Tiefenauer <daniel@tiefenauer.info>
2018-12-05 11:33:21 +0100

    fix boolean arguments

--- src/evaluate_pipeline_de.py
+++ src/evaluate_pipeline_de.py
@@ -39,14 +39,14 @@ parser.add_argument('--lm_path', type=str, required=False,
 parser.add_argument('--vocab_path', type=str, required=False,
                     help=f'(optional) Path to vocabulary file to use for spell checker.'
                     f'If not set you will be asked at runtime.')
-parser.add_argument('--force_realignment', type=bool, action='store_true',
+parser.add_argument('--force_realignment', action='store_true',
                     help='force realignment of partial transcript with original transcript, even if alignment'
                          'information is available from previous runs.')
-parser.add_argument('--align_endings', type=bool, action='store_true',
+parser.add_argument('--align_endings', action='store_true',
                     help='align endings of partial transcripts, not just beginnings. If set to True, transcript may'
                          'contain unaligned parts between alignments. If set to False, each alignment ends where the'
                          'next one starts.')
-parser.add_argument('--norm_transcript', type=bool, default=False,
+parser.add_argument('--norm_transcript', action='store_true',
                     help='Normalize transcript before alignment. If set to True, the alignments will be more accurate'
                          'because the transcript does not contain any punctuation, annotations and other clutter. '
                          'However, this might not reflect how the pipeline will be used. If set to False, the '
